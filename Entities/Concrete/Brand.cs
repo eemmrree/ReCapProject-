37c2748fe15f460d2d2975eef459e104bfb1de72
Emre <eemmrree0@outlook.com>
2021-10-28 18:15:32 +0300

    Entities'de Brand sınıfı oluşturuldu

--- Entities/Concrete/Brand.cs
+++ Entities/Concrete/Brand.cs
@@ -3,10 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Core.Entities;
 
 namespace Entities.Concrete
 {
-    public class Brand
+    public class Brand : IEntity
     {
         public int Id { get; set; }
         public string Name { get; set; }

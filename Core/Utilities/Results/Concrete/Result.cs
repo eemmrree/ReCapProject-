c2f9d1e0865995091aab2bc7cab97cd0a3e7e403
Emre <eemmrree0@outlook.com>
2021-10-29 14:41:39 +0300

    Core katmanında Result sınıfları eklendi.

---
+++ Core/Utilities/Results/Concrete/Result.cs
@@ -0,0 +1,20 @@
+﻿using Core.Utilities.Results.Abstract;
+
+namespace Core.Utilities.Results.Concrete
+{
+    public class Result:IResult
+    {
+        public Result(bool success, string message):this(success)
+        {
+            Message = message;
+        }
+        public Result(bool success)
+        {
+            Success = success;
+        }
+
+        //get = readonly ^ readonly ler constructor set edilebilir
+        public bool Success { get; }
+        public string Message { get; }
+    }
+}

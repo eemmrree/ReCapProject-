fbf8c5b0057ec1df3fd342e9a74eefe5a3012e47
Emre <eemmrree0@outlook.com>
2021-10-29 14:43:30 +0300

    Business'ta CarManager düzenlendi.

--- Business/Concrete/CarManager.cs
+++ Business/Concrete/CarManager.cs
@@ -3,10 +3,54 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Business.Abstract;
+using Core.Utilities.Results.Abstract;
+using Core.Utilities.Results.Concrete;
+using DataAccess.Abstract;
+using Entities.Concrete;
+using Entities.DTOs;
 
 namespace Business.Concrete
 {
-    public class CarManager
+    public class CarManager:ICarService
     {
+        readonly ICarDal _carDal;
+
+        public CarManager(ICarDal carDal)
+        {
+            _carDal = carDal;
+        }
+        public IDataResult<List<Car>> GetAll()
+        {
+            return new SuccessDataResult<List<Car>>(_carDal.GetAll(),"Listed");
+        }
+
+        public IDataResult<List<Car>> GetCarsByBrandId(int id)
+        {
+            return new SuccessDataResult<List<Car>>(_carDal.GetAll(p => p.BrandId == id));
+        }
+
+        public IDataResult<List<Car>> GetCarsByColorId(int id)
+        {
+            return new SuccessDataResult<List<Car>>(_carDal.GetAll(p => p.ColorId == id));
+        }
+
+        public IDataResult<List<CarDetailDto>> GetCarDetail()
+        {
+            return new SuccessDataResult<List<CarDetailDto>>(_carDal.GetCarDetails(),"Listed");
+        }
+
+        public IResult Add(Car car)
+        {
+            if (car.Description.Length >= 2 && car.DailyPrice > 0)
+            {
+                _carDal.Add(car);
+                return new SuccessResult("Added");
+            }
+            else
+            {
+                return new ErrorResult("Warning");
+            }
+        }
     }
 }

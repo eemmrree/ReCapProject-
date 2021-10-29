f219e186a3534360c9b448d4936475e80292426b
Emre <eemmrree0@outlook.com>
2021-10-29 14:44:02 +0300

    Test

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using Business.Concrete;
+using DataAccess.Concrete.EntityFramework;
 
 namespace ConsoleUI
 {
@@ -6,7 +8,25 @@ namespace ConsoleUI
     {
         static void Main()
         {
-            Console.WriteLine("Hello World!");
+            CarTest();
+        }
+
+        private static void CarTest()
+        {
+            var carManager = new CarManager(new EfCarDal());
+
+            foreach (var car in carManager.GetAll().Data)
+            {
+                Console.WriteLine(car.Id + " - " + car.BrandId + " - " + car.ColorId + " - " + car.DailyPrice + " - " + car.Description + " - " + car.ModelYear);
+            }
+
+            Console.WriteLine("---------------CarDetail-----------------");
+
+            foreach (var carDetail in carManager.GetCarDetail().Data)
+            {
+                Console.WriteLine(carDetail.Id +" - " +carDetail.BrandName+" - " +carDetail.ColorName+" -> " +carDetail.DailyPrice);
+            }
+
         }
     }
 }

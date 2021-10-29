70997e652861cffbcb577c1f82a848cc2df9666f
Emre <eemmrree0@outlook.com>
2021-10-29 14:40:21 +0300

    DataAccess EfCarDal updated.

--- DataAccess/Concrete/EntityFramework/EfCarDal.cs
+++ DataAccess/Concrete/EntityFramework/EfCarDal.cs
@@ -6,10 +6,21 @@ using System.Threading.Tasks;
 using Core.DataAccess.EntityFramework;
 using DataAccess.Abstract;
 using Entities.Concrete;
+using Entities.DTOs;
 
 namespace DataAccess.Concrete.EntityFramework
 {
     public class EfCarDal : EfEntityRepositoryBase<Car, ReCapProjectContext>, ICarDal
     {
+        public List<CarDetailDto> GetCarDetails()
+        {
+            using var context = new ReCapProjectContext();
+            var result = from p in context.Cars
+                join col in context.Colors on p.ColorId equals col.Id
+                join b in context.Brands on p.BrandId equals b.Id
+                select new CarDetailDto { Id = p.Id, BrandName = b.Name, ColorName = col.Name, DailyPrice = p.DailyPrice };
+
+            return result.ToList();
+        }
     }
 }

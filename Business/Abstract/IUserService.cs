dd1655f402750357e952516a239e896d92447b7b
Emre <eemmrree0@outlook.com>
2021-10-30 16:01:51 +0300

    Business'ta IUserService oluşturuldu.

---
+++ Business/Abstract/IUserService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Core.Utilities.Results.Abstract;
+using Entities.Concrete;
+
+namespace Business.Abstract
+{
+    public interface IUserService
+    {
+        IDataResult<List<User>> GetAll();
+        IResult Add(User user);
+        IResult Delete(User user);
+        IResult Update(User user);
+    }
+}

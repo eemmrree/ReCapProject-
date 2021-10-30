2392da53f0326deae14505be4cd1f852f481a43f
Emre <eemmrree0@outlook.com>
2021-10-30 16:00:04 +0300

    DataAccess'te EfRentalDal oluşturuldu.

---
+++ DataAccess/Concrete/EntityFramework/EfRentalDal.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Core.DataAccess.EntityFramework;
+using DataAccess.Abstract;
+using Entities.Concrete;
+using Entities.DTOs;
+
+namespace DataAccess.Concrete.EntityFramework
+{
+    public class EfRentalDal : EfEntityRepositoryBase<Rental, ReCapProjectContext>, IRentalDal
+    {
+        public List<RentalDetailDto> GetRentalDetails()
+        {
+            using var context = new ReCapProjectContext();
+            var result = from user in context.Users
+                join customer in context.Customers
+                    on user.Id equals customer.UserId
+                join rental in context.Rentals
+                    on customer.Id equals rental.CustomerId
+                join car in context.Cars
+                    on rental.CarId equals car.Id
+                join brand in context.Brands
+                    on car.BrandId equals brand.Id
+                join color in context.Colors
+                    on car.ColorId equals color.Id
+                select new RentalDetailDto
+                {
+                    Id = rental.Id,
+                    FirstName = user.FirstName,
+                    LastName = user.LastName,
+                    BrandName = brand.Name,
+                    ModelYear = car.ModelYear,
+                    ColorName = color.Name,
+                    DailyPrice = car.DailyPrice,
+                    RentDate = rental.RentDate,
+                    ReturnDate = rental.ReturnDate
+                };
+            return result.ToList();
+        }
+    }
+}

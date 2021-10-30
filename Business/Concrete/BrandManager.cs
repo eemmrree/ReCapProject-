c5a1613f3cacd465b49e3db0514c085452176aac
Emre <eemmrree0@outlook.com>
2021-10-30 14:10:03 +0300

    Business'ta BrandManager oluşturuldu.

---
+++ Business/Concrete/BrandManager.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Business.Abstract;
+using Core.Utilities.Results.Abstract;
+using Core.Utilities.Results.Concrete;
+using DataAccess.Abstract;
+using Entities.Concrete;
+
+namespace Business.Concrete
+{
+    public class BrandManager:IBrandService
+    {
+        readonly IBrandDal _brandDal;
+
+        public BrandManager(IBrandDal brand)
+        {
+            _brandDal = brand;
+        }
+
+        public IDataResult<List<Brand>> GetAll()
+        {
+            return new SuccessDataResult<List<Brand>>(_brandDal.GetAll());
+        }
+
+        public IResult Add(Brand brand)
+        {
+            if (brand.Name != null)
+            {
+                _brandDal.Add(brand);
+                return new SuccessResult("Added");
+            }
+            else
+            {
+                return new ErrorResult("Warning");
+            }
+        }
+
+        public IResult Delete(Brand brand)
+        {
+            _brandDal.Delete(brand);
+            return new SuccessResult("Deleted");
+        }
+
+        public IResult Update(Brand brand)
+        {
+            if (brand.Name != null)
+            {
+                _brandDal.Update(brand);
+                return new SuccessResult("Updated");
+            }
+            else
+            {
+                return new ErrorResult("Warning");
+            }
+        }
+    }
+}

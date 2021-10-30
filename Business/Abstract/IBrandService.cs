b2e2889b26c97200825684e43ef08bb64eba12dc
Emre <eemmrree0@outlook.com>
2021-10-30 14:09:42 +0300

    Business'ta IBrandService oluşturuldu.

---
+++ Business/Abstract/IBrandService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Core.Utilities.Results.Abstract;
+using Entities.Concrete;
+
+namespace Business.Abstract
+{
+    public interface IBrandService
+    {
+        IDataResult<List<Brand>> GetAll();
+        IResult Add(Brand brand);
+        IResult Delete(Brand brand);
+        IResult Update(Brand brand);
+    }
+}
